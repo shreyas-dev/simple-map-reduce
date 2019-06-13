a1c4761ca58fbcade4eaaffeb0414750c0f250e5
Shreyas B1 <shreyas.b1@olacabs.com>
2019-06-13 14:28:58 +0530

    Changes required

--- src/main/java/com/shreyas/dev/simple/mapreduce/WordCount.java
+++ src/main/java/com/shreyas/dev/simple/mapreduce/WordCount.java
@@ -6,6 +6,8 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.FileInputFormat;
 import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.TextInputFormat;
+import org.apache.hadoop.mapred.TextOutputFormat;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
@@ -14,21 +16,23 @@ import java.io.IOException;
 
 public class WordCount {
     public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        JobConf conf=new JobConf();
+        //Job Name
+        conf.setJobName("wordcount");
+        //Setting the output key and value
+        conf.setOutputKeyClass(Text.class);
+        conf.setOutputValueClass(IntWritable.class);
+        conf.setInputFormat(TextInputFormat.class);
+        conf.setOutputFormat(TextOutputFormat.class);
 
-        Configuration conf=new Configuration();
-        // Pass the cmd line args
-        // We will be running it as hadoop jar java.jar in out
-        // and out the convention java -jar , hence we need GenericOptionsParser
-        String[] otherArgs= new GenericOptionsParser(conf,args).getRemainingArgs();
 
+        //Input file location
+        FileInputFormat.setInputPaths(conf, new Path(args[0]));
 
-        if (otherArgs.length<2){
-            System.err.println("Usage:wordcount <in>[<in> ... ] <out>");
-            System.exit(1);
-        }
+        //Output file location
+        FileOutputFormat.setOutputPath(Job.getInstance(conf), new Path(args[1]));
+        Job job=new Job(conf);
 
-        // Creating a hadoop MR Job
-        Job job=new Job(conf,"word count");
         //Setting the Main Class which contains main method
         job.setJarByClass(WordCount.class);
         //Mapper Class
@@ -39,15 +43,6 @@ public class WordCount {
         //Setting the output key and value
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
-
-        JobConf jobConf=new JobConf(conf);
-        // Multiple Input Files
-        for (int i=0;i<otherArgs.length-1;i++){
-            FileInputFormat.addInputPath(jobConf,new Path(otherArgs[i]));
-        }
-        //Output file location
-        FileOutputFormat.setOutputPath(job,new Path(otherArgs[otherArgs.length-1]));
-
 
         //job.waitForCompletion will submit the job to map-reduce
         System.exit(job.waitForCompletion(true) ? 0 : 1 );
